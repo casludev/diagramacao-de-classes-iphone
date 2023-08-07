22aaae2023562662796c3b3018e571e4e008ad6a
[casludev] <deadlordd@gmail.com>
2023-08-07 00:26:06 -0300

    Comit final

---
+++ src/iPhone.java
@@ -0,0 +1,13 @@
+public class iPhone {
+    private String modelo;
+    private String cor;
+    private int capacidade;
+
+    // implementações de ligar e desligar
+    public void ligar(){
+
+    }
+    public void desligar(){
+
+    }
+}
